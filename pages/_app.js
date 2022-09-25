6a4594c0d4fd95b61ba2bfb0fd3af78ee336d0d3
i <iraklishanava01@gmail.com>
2022-09-25 12:11:16 +0400

    Added property detail page,fixed filtering button bug

--- pages/_app.js
+++ pages/_app.js
@@ -5,14 +5,15 @@ import { ChakraProvider } from "@chakra-ui/react";
 
 import Layout from "../components/Layout";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) { 
+
   NProgress.configure({ showSpinner: false });
 
-  Router.events.on("routeChangeStart", () => {
+  Router.events.on('routeChangeStart', () => {
     NProgress.start();
   });
 
-  Router.events.on("routeChangeComplete", () => {
+  Router.events.on('routeChangeComplete', () => {
     NProgress.done();
   });
 
